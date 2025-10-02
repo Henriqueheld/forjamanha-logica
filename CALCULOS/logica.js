d2d19cc50d2ccc658bf213530d24a900795b3a1a
Henriqueheld <henrique.k.almeida@aluno.senai.br>
2025-10-02 16:51:50 -0300

    Add files via upload

---
+++ CALCULOS/logica.js
@@ -0,0 +1,48 @@
+function soma(){
+    var numero1 = document.getElementById("n1").valueAsNumber 
+    var numero2 = document.getElementById("n2").valueAsNumber
+    var resposta = document.getElementById("respSoma")
+    resposta.textContent = numero1 + numero2
+}
+
+function sub(){
+    var num1 = parseFloat(document.getElementById("n3").value)
+    var num2 = parseFloat(document.getElementById("n4").value)
+    document.getElementById("respSub").textContent = num1 - num2
+}
+
+function multi(){
+    var number1 = parseFloat(document.getElementById("n5").value)
+    var number2 = parseFloat(document.getElementById("n6").value)
+    document.getElementById("respMulti").textContent = number1 * number2
+}
+
+function div(){
+    var a = parseFloat(document.getElementById("n7").value)
+    var b = parseFloat(document.getElementById("n8").value)
+    var resp = document.getElementById("respDiv")
+
+    if(b !== 0){
+        resp.innerHTML = (a / b).toFixed(2)
+    }else{
+        resp.innerHTML = "Não se divide por 0"
+    }
+}
+
+    /* animar titulo h1 */
+
+var titulo = document.querySelector("h1")
+var cor = 0
+
+function animarTitulo() {
+    cor += 5
+    titulo.style.color = "hsl(" + cor + ", 100%, 50%)"
+    if(cor < 360){
+        requestAnimationFrame(animarTitulo)
+    }else{
+        cor = 0
+        requestAnimationFrame(animarTitulo)
+    }
+}
+
+animarTitulo()
