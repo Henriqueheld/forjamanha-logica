12a91c15ca1b6a84bdb7dbeba4c3cf9a4f6a8fd3
Henriqueheld <henrique.k.almeida@aluno.senai.br>
2025-10-22 16:41:53 -0300

    Add files via upload

---
+++ CALCULOS/Revisao/script.js
@@ -0,0 +1,45 @@
+// console.log("Hello Word!")
+
+// Variaveis 
+
+//  var fruta = 3  // É uma varivel que pode  ser atualizada
+//  var fruta = "abacate"
+// console.log(typeof fruta, fruta)
+
+// let nome = "batata"
+// nome = "batata"
+// let nome = "andre"
+
+// const tem = true
+// const tem = "andre"
+// console.log(typeof tem, tem)
+
+// var preco = 25
+// var quantidade = 5
+// var total = preco*quantidade
+
+
+//concatenar strings e variaveis
+// var resposta =  "este preço vezes " + preco + " vezes este valor " + quantidade + " é resultante = " + total
+// console.log(resposta)
+
+//tamplate string
+// var resposta = `Este precço ${preco} vezes este valor ${quantidade} = ${total}`
+
+// console.log(resposta)
+
+var nome = "Henrique"
+var idade = 16
+
+var tudoJunto = `Meu nome é ${nome} e eu tenho ${idade} anos.`
+var outrojeito = "Meu nome é " + nome + " e eu tenho " + idade + " anos."
+
+
+console.log(outrojeito)
+
+
+
+
+
+
+
